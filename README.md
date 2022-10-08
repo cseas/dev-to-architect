cb7ec930d069c6b8a7813ca389eb89a0a6031247
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-10-08 18:40:20 +0530

    refactor: inOrder DFS using recursion

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # dev-to-architect
+
 Code from Percipio Aspire Journey: Developer to Software Architect
+
+## Tracks
+
+- Track 1: Developer
+  - Advanced Data Structures & Algorithms in Java
+    - [ ] Working with Binary Trees
+    - [ ] Solving Binary Tree Problems
+    - [ ] Working with Binary Search Trees
+    - [ ] Sorting & Searching Algorithms
+    - [ ] Working with the Binary Heap
+    - [ ] Getting started with Graphs
+    - [ ] Working with Graph Algorithms
+  - Multi-threading and Concurrency in Java
+    - [ ] Introduction to Concurrent Programming
+    - [ ] Spawning & Launching Threads
+    - [ ] Thread Synchronization & Locks
+    - [ ] Concurrency & the Producer-Consumer Problem
+    - [ ] Objects for Concurrent Programming
+    - [ ] Scaling a Multi-threaded Application
+- Track 2: Database Developer
+- Track 3: Full Stack Developer
+- Track 4: Software Architect
--- track1-developer/src/Dfs.java
+++ track1-developer/src/Dfs.java
@@ -1,38 +1,12 @@
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
-
 public class Dfs {
   public static <T> void inOrder(Node<T> root) {
     if (root == null) {
       return;
     }
 
-    Set<Node<T>> visitedNodes = new HashSet<>();
-    Stack<Node<T>> stack = new Stack<>();
-    stack.push(root);
-
-    while (!stack.isEmpty()) {
-      Node<T> top = stack.pop();
-
-      if (top.getLeftChild() == null && top.getRightChild() == null) {
-        System.out.print(top + " -> ");
-      } else if (visitedNodes.contains(top)) {
-        System.out.print(top + " -> ");
-      } else {
-        visitedNodes.add(top);
-
-        if (top.getRightChild() != null) {
-          stack.push(top.getRightChild());
-        }
-
-        stack.push(top);
-
-        if (top.getLeftChild() != null) {
-          stack.push(top.getLeftChild());
-        }
-      }
-    }
+    inOrder(root.getLeftChild());
+    System.out.print(root.getData() + " -> ");
+    inOrder(root.getRightChild());
   }
 
   public static void main(String[] args) {
