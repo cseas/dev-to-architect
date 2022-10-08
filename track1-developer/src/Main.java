2f44503067c98ac4f3525bdb2e55b819e3b708a4
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-10-08 17:38:20 +0530

    feat: add levels of nodes being traversed

--- track1-developer/src/Main.java
+++ track1-developer/src/Main.java
@@ -7,19 +7,25 @@ public class Main {
       return;
     }
 
-    Queue<Node<T>> queue = new LinkedList<>();
-    queue.add(root);
+    Queue<Pair<Node<T>, Integer>> queue = new LinkedList<>(); // Pair(data of node, level of node)
+
+    int level = 0;
+    queue.add(new Pair<>(root, level));
 
     while (!queue.isEmpty()) {
-      Node<T> node = queue.remove();
-      System.out.print(node + " -> ");
+      Pair<Node<T>, Integer> pair = queue.remove();
+      System.out.print(pair + " -> ");
 
-      if (node.getLeftChild() != null) {
-        queue.add(node.getLeftChild());
+      level = pair.getValue() + 1;
+
+      Node<T> leftChild = pair.getKey().getLeftChild();
+      if (leftChild != null) {
+        queue.add(new Pair<>(leftChild, level));
       }
 
-      if (node.getRightChild() != null) {
-        queue.add(node.getRightChild());
+      Node<T> rightChild = pair.getKey().getRightChild();
+      if (rightChild != null) {
+        queue.add(new Pair<>(rightChild, level));
       }
     }
   }
